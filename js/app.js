e68e8507480b1502af8168bd56fc32cebedf1621
Nihat <abdullazadenihat@gmail.com>
2022-11-12 15:06:42 +0400

    new mode added

--- js/app.js
+++ js/app.js
@@ -1,70 +1,110 @@
+"use strict";
 const Game = {
-   correctAnswers: 0,
-   wrongAnswers: 0,
+   settings: {
+      region: ['Europe'],
+      mode: 'Timer',
+   },
+   correctAnswer: 0,
+   wrongAnswer: 0,
    variants: {
       count: 4,
-      Initials: ['A)', 'B)', 'C)', 'D)', 'E)'],
-   },
-   questionArr: [],
-}
-let data;
+      initials: {
+         standart: ['A)', 'B)', 'C)', 'D)']
 
-fetch("api/flags.json").then(res => res.json()).then(res => { data = res; loadQuestion() });
-
-
-function getRandomCountry() {
-   return data[Math.floor(Math.random() * data.length)];
-}
-
-function generateVariants(country) {
-   let countryArr = [];
-   for (let i = 1; i < Game.variants.count; i++) {
-      let isSame = false;
-      let country_ = getRandomCountry();
-      if (country.code == country_.code) {
-         i--;
-         continue;
       }
+   },
+   data: null,
+   question: {
+      'title': "Choose the correct variant",
+      answer: '',
+      variants: [],
+   }
+}
 
-      for (let j = 0; j < country_.length; j++) {
-         if (country_[j].code == country.code) {
-            i--;
-            isSame = true
-            break;
+let timer = 60;
+let correctAnswerEl = document.querySelector(".question .correct-answer-count span")
+let timerEl = document.querySelector(".question .timer span");
+
+fetch('./api/all.json').then(res => res.json()).then(data => { Game.data = data; main() })
+
+function getCountry() {
+   while (true) {
+      let z = Game.data[Math.floor(Math.random() * Game.data.length)];
+      for (let i = 0; i < Game.settings.mode.length; i++) {
+         if (z.region == Game.settings.region[i]) {
+            return z
          }
       }
-      if (isSame) {
-         continue;
+   }
+}
+
+function getQuestion() {
+   document.querySelector('.question .variants').innerHTML = ""
+   Game.question.answer = getCountry();
+   Game.question.variants = [];
+
+   document.getElementById('flag').src = Game.question.answer.flags.svg;
+
+   for (let i = 0; i < Game.variants.count - 1; i++) {
+      Game.question.variants.push(getCountry());
+   }
+   Game.question.variants.splice(Math.round(Math.random(Game.variants.count)), 0, Game.question.answer);
+
+
+   for (let i = 0; i < Game.question.variants.length; i++) {
+      let btn = document.createElement('button');
+      btn.onclick = nextQuestion;
+      btn.className = 'answer';
+      btn.dataset.name = Game.question.variants[i].name.common
+      btn.innerText = Game.variants.initials.standart[i] + " " + Game.question.variants[i].name.common
+      document.querySelector('.variants').append(btn);
+   }
+
+   // console.log(Game.question.answer.name.common, Game.question.variants);
+
+}
+
+function main() {
+   getQuestion();
+   // startGame();
+}
+
+
+function startGame() {
+   let region = [];
+   document.getElementsByName('region').forEach(r => {
+      if (r.checked) {
+         region.push(r.value)
       }
-      countryArr.push(country_);
-
+   })
+   if (region.length == 0) {
+      return false;
    }
-   countryArr.splice(1, 0, country)
-   return countryArr;
+   Game.settings.region = region;
+
+   document.querySelectorAll('.screen').forEach(s => {
+      s.style.display = "none"
+   })
+   getQuestion();
+   document.querySelector('.screen-question').style.display = 'block';
+
+   let timerId = setInterval(() => {
+      if (timer > 0) {
+         timerEl.innerText = timer + " seconds"
+         timer--;
+      } else {
+         clearInterval(timerId);
+      }
+
+   }, 1000);
 
 }
 
-function loadQuestion() {
-   let country = getRandomCountry()
-   let countryArr = generateVariants(country)
-   Game.questionArr.push(country);
-
-
-
-   document.querySelector(".flag-wrap img").src = country.flag.w80
-
-   for (let i = 0; i < Game.variants.count; i++) {
-      document.querySelector(".variants").innerHTML +=
-         ` <button> ${Game.variants.Initials[i]}  ${countryArr[i].name} </button> `
-
+function nextQuestion() {
+   if (this.dataset.name == Game.question.answer.name.common) {
+      Game.correctAnswer++;
+      correctAnswerEl.innerText = Game.correctAnswer + " correct";
    }
-
-
-
-   // country_.forEach(o => {
-   //    document.querySelector('.variants').innerHTML += o.name
-   // })
+   getQuestion();
 
 }
-
-
